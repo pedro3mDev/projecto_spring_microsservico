41f83ceb1a536c39d6ee9345832cec87e8aed117
Pedro Moniz <88067976+pedro3mDev@users.noreply.github.com>
2024-06-19 16:17:12 +0100

    ...

---
+++ projecto/src/main/java/grupo3mtech/ao/projecto/ProjectoApplication.java
@@ -0,0 +1,13 @@
+package grupo3mtech.ao.projecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoApplication.class, args);
+	}
+
+}
---
+++ projecto/src/test/java/grupo3mtech/ao/projecto/ProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3mtech.ao.projecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
